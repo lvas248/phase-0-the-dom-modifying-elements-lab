3855dfb856b10f7a37434cd37e9094cf8a85c747
Luis <lvas248@gmail.com>
2022-06-01 16:55:03 -0400

    commit tree ab0173afdd889a47af50aa43b493286ff3a93b28

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.querySelector('main').remove();
+
+const newHeader = document.createElement('h1')
+newHeader.textContent = "YOUR-NAME is the champion"
+newHeader.id = "victory"
+
+document.body.apppend(newHeader)
